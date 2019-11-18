98b2022e0170de728b8c3cd59fc0472f249278ae
Claudio <claudiomano@live.it>
2019-11-18 23:21:40 +0100

    Aggiunti TE ed Overview a data

--- Data.java
+++ Data.java
@@ -1,3 +1,14 @@
-public interface Data {
+public interface Data<E> {
+    
+    /**
+     * Overview:    dato di un qualsiasi tipo sul quale può essere applicato il metodo display
+     * 
+     * TE:          <element, category, likes>
+     */
+
+    public void updateData(E el);
+
+    public E getData();
+
     public void Display();
 }
