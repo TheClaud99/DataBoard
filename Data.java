4c1458bbaeec124d687a2a3614fc23f3640bcf55
c.mano <c.mano@progresso.serl>
2019-11-21 09:02:03 +0100

    Modificato Typical Element Data

--- Data.java
+++ Data.java
@@ -3,12 +3,22 @@ public interface Data<E> {
     /**
      * Overview:    dato di un qualsiasi tipo sul quale può essere applicato il metodo display
      * 
-     * TE:          <element, category, likes>
+     * TE:          <element, likes>, element != null and likes > 0
      */
 
+    // Cambia il valore di el
+    /**
+     * 
+     * @param el, el != null
+     * @throws NullPointerException
+     * @effects post(this.element) = el
+     * 
+     */
     public void updateData(E el);
 
+    // Prende il valore di element
     public E getData();
 
+    // Stampa il valore di element
     public void Display();
 }
